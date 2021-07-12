a9acedc809e994a7083bf2233367caeb69536976
Ajay Kumar Reddy Kanduru <63453305+AjayReddyK@users.noreply.github.com>
2021-07-12 14:01:55 +0530

    home page updated

--- user/views.py
+++ user/views.py
@@ -8,7 +8,7 @@ from django.contrib import messages
 
 context={}
 hubcontent={}
-
+s=requests.Session()
 def loginhome(request):
 	headers={'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'}
 	if request.method=='POST':
@@ -22,7 +22,8 @@ def loginhome(request):
    			'rememberusername':'1'
 		}
 		print("credentials received")
-		with requests.Session() as s:
+		global s
+		with s:
 			  attendance_subjects=[]
 			  attendance_timings=[]
 			  attendance_status=[]
@@ -39,9 +40,9 @@ def loginhome(request):
 			  global context
 			  print("entering requests")
 			  url="http://lms.rgukt.ac.in/login/index.php"
-			  r=s.get(url,headers=headers).text
+			  r=s.get(url,headers=headers)
 			  print("got data, appling soup")
-			  soup=BeautifulSoup(r,'lxml')
+			  soup=BeautifulSoup(r.content,'html5lib')
 			  print("soup applied , next finding logintoken")
 			  login_data['logintoken']=soup.find('input',attrs={'name':'logintoken'})['value']
 			  print("found token , next posting request")
@@ -122,7 +123,7 @@ def loginhome(request):
 				  				"sessid":sessid,
 				  				"sesskey":sesskey,
 				  				"sesskey":sesskey,
-				  				"qf_mod_attendance_form_studentattendance":1,
+				  				"_qf__mod_attendance_form_studentattendance":1,
 				  				"mform_isexpanded_id_session":1,
 				  				"status":status,
 				  				"submitbutton":"Save+changes"
@@ -271,9 +272,114 @@ def loginhome(request):
 	return render(request,'user/login.html',hubcontent)
 def home(request):
 	global context
+	global s
+	attendance_subjects=[]
+	attendance_timings=[]
+	attendance_status=[]
+	content=[]
+	headers={'user-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:89.0) Gecko/20100101 Firefox/89.0'}
+	with s:
+		time=str(datetime.now()).split(" ")[1].split(":")[:2]
+		for i in range(2):
+	  		time[i]=int(time[i])
+		time[0]=time[0]+5
+		time[1]=time[1]+30
+		if(time[1]>=60):
+	  		dif=time[1]-60
+	  		time[1]=dif
+	  		time[0]+=1
+		print("current time=",time)
+		cal=s.get("http://lms.rgukt.ac.in/calendar/view.php?view=day",headers=headers).text
+		soup=BeautifulSoup(cal,'lxml')
+		date=soup.find('div',class_="calendar-controls").h2.text.split(" ")
+		date[2]=date[2][0:3]
+		date=" ".join(date[1:])
+		print(date)
+		list=soup.find('body')
+		l2=list.find('div',{"class":"calendarwrapper"})
+		l3=l2.find("div",class_="eventlist my-1")
+		events=l3.find_all('div',{'class':'event mt-3','data-event-component':'mod_attendance'})
+		for i in events:
+	  		timet=i.find("div",class_="row").find("div",class_="col-11").text
+	  		updated_time=timet.split(",")[1]
+	  		attendance_timings.append(updated_time)
+	  		print("updated_time=",updated_time)
+	  		start_t=updated_time[1:].split(" ")[:2]
+	  		end_t=updated_time[1:].split(" ")[3:]
+	  		print("start_time=",start_t)
+	  		print("end_time=",end_t)
+	  		start_time=start_t[0].split(":")
+	  		start_time.append(start_t[1])
+	  		end_time=end_t[0].split(":")
+	  		end_time.append(end_t[1])
+	  		for j in range(2):
+	  			start_time[j]=int(start_time[j])
+	  			end_time[j]=int(end_time[j])
+	  		if(start_time[2]=="PM" and start_time[0]<12):
+	  			start_time[0]=start_time[0]+12
+	  		if(end_time[2]=="PM" and end_time[0]<12):
+	  			end_time[0]=end_time[0]+12
+	  		event=i.find_all('div',class_='row mt-1')[-1]
+	  		subject_name=event.find('div',class_='col-11').a.text
+	  		print("total start time=",start_time)
+	  		print("start time=",start_time[:2])
+	  		print("total end time=",end_time)
+	  		print("end time=",end_time[:2])
+	  		attendance_subjects.append(subject_name)
+	  		if(time[0]<start_time[0]):
+	  			attendance_status.append("Not active yet")
+	  		elif(time[0]==start_time[0] and time[1]<start_time[1]):
+	  			attendance_status.append("Not active yet")
+	  		else:
+		  		link=i.find('a',class_='card-link')['href']
+		  		link=s.get(link,headers=headers).text
+		  		soup=BeautifulSoup(link,'lxml')
+		  		a_link=soup.find('td',class_="statuscol cell c2 lastcol")
+		  		if(a_link!=None):
+		  			a_link=a_link.a['href']
+		  			b=a_link.split("?")[1].split("&")
+		  			sessid=b[0].split("=")[1]
+		  			sesskey=b[1].split("=")[1]
+		  			print(sesskey)
+		  			link="http://lms.rgukt.ac.in/mod/attendance/attendance.php"
+		  			final_page=s.get(a_link,headers=headers).text
+		  			soup=BeautifulSoup(final_page,'lxml')
+		  			status=soup.find('input',{"class":"form-check-input","name":"status"})['value']
+		  			print("status code=",status)
+		  			at_data={
+		  				"sessid":sessid,
+		  				"sesskey":sesskey,
+		  				"sesskey":sesskey,
+		  				"_qf__mod_attendance_form_studentattendance":1,
+		  				"mform_isexpanded_id_session":1,
+		  				"status":status,
+		  				"submitbutton":"Save+changes"
+		  			}
+		  			headers['referer']=a_link
+		  			submission=s.post(link,data=at_data,headers=headers,allow_redirects=True)
+		  			attendance_status.append("Attendance Marked... [^_^]")
+		  			print("attendance marked")
+		  		else:
+		  			m=soup.find(text=re.compile(date)).parent.parent.parent
+		  			att_status=m.find('td',class_="statuscol cell c2").text
+		  			if(att_status=="?"):
+		  				if(time[0]<end_time[0] or (time[0]==end_time[0] and time[1]<end_time[1])):
+		  					attendance_status.append("Not active yet(server time lag-reload after sometime)")
+		  				else:
+		  					attendance_status.append("absent")
+		  			elif(att_status=="Present"):
+		  			 	attendance_status.append("present")
+		  			else:
+		  				attendance_status.append("status_unknown")
+		for i in range(len(attendance_subjects)):
+	  		dik={}
+	  		dik['subject']=attendance_subjects[i]
+	  		dik['time']=attendance_timings[i]
+	  		dik['status']=attendance_status[i]
+	  		content.append(dik)
+		context['attendance']=content
 	return render(request,'user/success_message.html',context)
 def hub(request):
 	global hubcontent
-	hubcontent['login']="true"
 	return render(request,'user/hub.html',hubcontent)
 # Create your views here.
