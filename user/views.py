52af8d6288180729aef7853bb1a737911511c4a5
thoutamganesh66 <ganeshgb1024@gmail.com>
2021-07-13 19:10:03 +0530

    stable version 1.2

--- user/views.py
+++ user/views.py
@@ -42,9 +42,9 @@ def loginhome(request):
               global context
               print("entering requests")
               url="http://lms.rgukt.ac.in/login/index.php"
-              r=s.get(url,headers=headers)
+              r=s.get(url,headers=headers).text
               print("got data, appling soup")
-              soup=BeautifulSoup(r.content,'html5lib')
+              soup=BeautifulSoup(r,'lxml')
               print("soup applied , next finding logintoken")
               login_data['logintoken']=soup.find('input',attrs={'name':'logintoken'})['value']
               print("found token , next posting request")
