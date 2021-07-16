82a4bcbf5ac7fc2eb32f0616e72004cda3de24a2
Ajay Kumar Reddy Kanduru <ajay45177@gmail.com>
2021-07-16 19:25:52 +0530

    admin updated

--- user/models.py
+++ user/models.py
@@ -3,5 +3,6 @@ from django.db import models
 class Profile(models.Model):
   b_id=models.CharField(max_length=20)
   device=models.CharField(max_length=400)
-
+  def __str__(self):
+    return self.b_id
 # Create your models here.
